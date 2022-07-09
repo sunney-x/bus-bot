0713b84345a501826318a73982a6b40472cee5e7
sunney-x <sanishubitidze@live.com>
2022-07-09 22:38:10 +0400

    Implemented keyboards

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,8 @@
+import type { BusWithStopID } from "./utils";
 import { Markup, Telegraf } from "telegraf";
-import { createClient } from "redis";
 import dotenv from "dotenv";
+import * as utils from "./utils";
 import ttc from "ttc-api";
-import { getArrivalTime } from "./utils";
 
 dotenv.config();
 
@@ -11,17 +11,122 @@ if (!process.env.TELEGRAM_TOKEN) {
 }
 
 const bot = new Telegraf(process.env.TELEGRAM_TOKEN);
+const { users } = utils;
 
-bot.command("start", (ctx) =>
+bot.start((ctx) =>
   ctx.reply(
-    "პრივეტ",
+    "TTC 🚏",
     Markup.keyboard([
-      Markup.button.text("ჩემი გაჩერებების ნახვა"),
-      Markup.button.text("ახალი გაჩერების დამატება"),
+      Markup.button.locationRequest("ავტობუსების ნახვა 🔍"),
+      Markup.button.text("ძებნის რადიუსის შეცვლა ✏️"),
     ]).resize()
   )
 );
 
-bot.hears("ჩემი გაჩერებების ნახვა", (ctx) => ctx.reply(":)"));
+bot.hears("ძებნის რადიუსის შეცვლა ✏️", (ctx) => {
+  ctx.reply(
+    `მოუმატე ან მოუკლე ძებნის რადიუსს 😊\nამჟამანდელი რადიუსი: ${users.get(
+      ctx.from.id
+    )}`,
+    Markup.inlineKeyboard([
+      Markup.button.callback("მოუმატე", "add-radius"),
+      Markup.button.callback("მოუკლე", "sub-radius"),
+    ])
+  );
+});
+
+bot.action("add-radius", async (ctx) => {
+  ctx.from && users.add(ctx.from?.id);
+  await ctx.editMessageText(
+    `მოუმატე ან მოუკლე ძებნის რადიუსს 😊\nამჟამანდელი რადიუსი: ${users.get(
+      ctx.from?.id
+    )}`,
+    Markup.inlineKeyboard([
+      Markup.button.callback("მოუმატე", "add-radius"),
+      Markup.button.callback("მოუკლე", "sub-radius"),
+    ])
+  );
+});
+
+bot.action("sub-radius", async (ctx) => {
+  ctx.from && users.sub(ctx.from?.id);
+  await ctx.editMessageText(
+    `მოუმატე ან მოუკლე ძებნის რადიუსს 😊\nამჟამანდელი რადიუსი: ${users.get(
+      ctx.from?.id
+    )}`,
+    Markup.inlineKeyboard([
+      Markup.button.callback("მოუმატე", "add-radius"),
+      Markup.button.callback("მოუკლე", "sub-radius"),
+    ])
+  );
+});
+
+bot.on("location", async (ctx) => {
+  const location = ctx.message.location;
+  const lat = location.latitude;
+  const lng = location.longitude;
+  const radius = users.get(ctx.from?.id);
+
+  const nearbyStops = await utils.searchNearbyBusStops(radius, lat, lng);
+  const timeTables = await utils.fetchTimeTables(nearbyStops);
+
+  const busList = timeTables
+    .filter((bus) => bus.code !== undefined)
+    .map((busStop) =>
+      busStop.arrivalTimes.map((at) => ({
+        ...at,
+        stopID: busStop.code!,
+      }))
+    )
+    .flat();
+
+  const busListWithDistance = Object.values(
+    busList.reduce(
+      (
+        acc: {
+          [key: string]: BusWithStopID;
+        },
+        bus
+      ) => {
+        if (!acc[bus.DestinationStopName]) {
+          acc[bus.DestinationStopName] = bus;
+        }
+
+        return acc;
+      },
+      {}
+    )
+  );
+
+  ctx.reply(
+    `ავტობუსები ${radius} მეტრის რადიუსში`,
+    Markup.inlineKeyboard(
+      busListWithDistance.map((bus) =>
+        Markup.button.callback(
+          bus.DestinationStopName,
+          `${bus.RouteNumber}:${bus.stopID}`
+        )
+      ),
+      {
+        columns: 2,
+      }
+    )
+  );
+});
+
+bot.action(/(.*):(.*)/, async (ctx) => {
+  if (!ctx.callbackQuery.data) return ctx.reply("მონაცემი ვერ მოიძებნა 😢");
+
+  const [busNumber, stopId] = ctx.callbackQuery.data.split(":");
+
+  const stops = await utils.busStops();
+
+  const isForward = await utils.isBusForward(busNumber, stopId);
+  const busStop = stops.find((stop) => stop.code === stopId);
+
+  const loc = ttc.busLiveLocations(busNumber, isForward);
+
+  // ctx.replyWithLocation(busStop.location, {
+});
 
 bot.launch();
--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,38 @@
+import type { ArrivalInfo, Stop } from "ttc-api/lib/types";
 import ttc from "ttc-api";
+
+export let stops: Stop[] = [];
+
+const _users: { [key: number]: number } = {};
+
+export const users = {
+  add: (id?: number) => {
+    if (typeof id === "undefined") return 0;
+    _users[id] = _users[id] + 100;
+    return _users[id];
+  },
+  sub: (id?: number) => {
+    if (typeof id === "undefined") return 0;
+    _users[id] = _users[id] - 100;
+    return _users[id];
+  },
+  get: (id?: number) => {
+    if (typeof id === "undefined") return 0;
+    _users[id] = _users[id] || 500;
+    return _users[id];
+  },
+};
+
+export type StopWithDistance = Stop & { distance: number };
+export type BusWithStopID = ArrivalInfo & { stopID: string };
+
+export const busStops = async (): Promise<Stop[]> => {
+  if (stops.length > 0) return stops;
+
+  stops = await ttc.stops();
+
+  return stops;
+};
 
 export const getArrivalTime = async (busId: string, stopId: string) => {
   const arrivalTimes = await ttc.stopArrivalTimes(stopId);
@@ -12,4 +46,70 @@ export const getArrivalTime = async (busId: string, stopId: string) => {
   const arrivalTime = bus.ArrivalTime;
 
   return arrivalTime;
+};
+
+export const searchNearbyBusStops = async (
+  radius: number,
+  lat: number,
+  lon: number
+): Promise<StopWithDistance[]> => {
+  const s = performance.now();
+  const stops = await busStops();
+  console.log(`stops: ${performance.now() - s}`);
+
+  return stops
+    .reduce((acc, stop) => {
+      const distance = distanceBetweenTwoPoints(lat, lon, stop.lat, stop.lon);
+
+      if (distance <= radius) {
+        acc.push({
+          ...stop,
+          distance,
+        });
+      }
+
+      return acc;
+    }, [] as StopWithDistance[])
+    .sort((a, b) => a.distance - b.distance);
+};
+
+export const fetchTimeTables = async (busStops: Stop[]) => {
+  const timeTables = await Promise.all(
+    busStops.map(async (busStop) => {
+      const arrivalTimes = await ttc.stopArrivalTimes(busStop.id.split(":")[1]);
+
+      return {
+        ...busStop,
+        arrivalTimes,
+      };
+    })
+  );
+
+  return timeTables;
+};
+
+export const isBusForward = async (busNumber: string, stopId: string): Promise<boolean> => {
+  const yes = await ttc.busRouteInfo(busNumber, true);
+
+  return yes.RouteStops.some((stop) => stop.StopId === stopId);
+};
+
+export const distanceBetweenTwoPoints = (
+  lat1: number,
+  lon1: number,
+  lat2: number,
+  lon2: number
+): number => {
+  const R = 6371e3; // metres
+  const φ1 = (lat1 * Math.PI) / 180;
+  const φ2 = (lat2 * Math.PI) / 180;
+  const Δφ = ((lat2 - lat1) * Math.PI) / 180;
+  const Δλ = ((lon2 - lon1) * Math.PI) / 180;
+
+  const a =
+    Math.sin(Δφ / 2) * Math.sin(Δφ / 2) +
+    Math.cos(φ1) * Math.cos(φ2) * Math.sin(Δλ / 2) * Math.sin(Δλ / 2);
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+  return Math.floor(R * c);
 };
